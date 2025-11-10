392d32445cc8a74de2a2ba67d36693d4cbe14627
Antonio Salces Alcaraz <antoniosalces06@gmail.com>
2025-11-10 23:01:39 +0100

    relacion 2 y reestructuracion de carpetas

---
+++ Relacion02/Relacion02_Ejercicio07_ConCaptura/Relacion02_Ejercicio07_ConCaptura/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Relacion02_Ejercicio07_ConCaptura
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BotonExcepcion_Click(object sender, RoutedEventArgs e)
+        {
+            int[] betisArray = new int[3];
+            try
+            {
+                betisArray[10] = 1914;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show("¡Error bético! Se intentó acceder a una posición inválida del array.\nDetalle: " + ex.Message,
+                    "Excepción controlada", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+    }
+}
